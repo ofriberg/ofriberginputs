37b397cb769ccee0c4299a926b21e7c4438a8c87
Oscar Friberg <oscfriberg@gmail.com>
2020-10-11 18:04:04 +0200

    readme

--- Button.js
+++ Button.js
@@ -2,20 +2,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import * as S from './Button.styled'
 
-const Button = props => {
+const Button = ({onClick, title}) => {
     return (
         <S.Container>
-            <S.Button onClick={handleClick}>{title}</S.Button>
+            <S.Button onClick={onClick}>{title}</S.Button>
         </S.Container>
     )
 }
 
 Button.propTypes = {
-    handleClick: PropTypes.func,
+    onClick: PropTypes.func,
     title: PropTypes.string
 }
 Button.defaultProps = {
-    handleClick: () => null,
+    onClick: () => null,
     title: 'btn'
 }
 
